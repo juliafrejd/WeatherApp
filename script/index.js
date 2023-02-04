3eebf2ad1374351b9bd00a2eba5c9cdc11ea6b18
juliafrejd <118212457+juliafrejd@users.noreply.github.com>
2023-02-04 10:14:26 +0000

    Created test-function for forecast
    Weather forecast moved to JS, created function, test days, looping with foreach.

--- script/index.js
+++ script/index.js
@@ -34,6 +34,33 @@ function todaysDate() {
 }
 todaysDate();
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+      <div class="weather-forecast-day">${day}</div>
+      <img
+        src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png"
+        alt="weather-icon"
+        width="36"
+      />
+      <div class="weather-forecast-temp">
+        <span class="weather-forecast-temperature-max">17°</span>
+        <span class="weather-forecast-temperature-min">/ 10°</span>
+      </div>
+    </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showFahrenheit(event) {
   event.preventDefault();
   let currentTemperature = document.querySelector("#showtemp");
@@ -76,7 +103,7 @@ function showWeather(response) {
   changeDescription.innerHTML = `${description}`;
   changeIcon.setAttribute(
     "src",
-    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+    `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
   changeIcon.setAttribute("alt", response.data.condition.description);
 }
@@ -113,3 +140,4 @@ let searchButton = document.querySelector("#search-button");
 searchButton.addEventListener("click", searchClick);
 
 let celsiusTemperature = null;
+displayForecast();
